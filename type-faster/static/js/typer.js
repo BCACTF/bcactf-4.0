c47178b6cef16ce78add81fcfd3fde236b58d541
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-06-05 15:37:26 -0400

    fixed the big problem thing where the thing doesnt work

--- type-faster/static/js/typer.js
+++ type-faster/static/js/typer.js
@@ -1,25 +1,20 @@
 const typer = document.getElementById('typewriter');
-typer.value = '';
+typer.innerText = '';
 
-window.addEventListener('load', function () {
-    let time = new Date().getTime();
-    fetch('/times', {
-        method: 'POST',
-        body: JSON.stringify({
-            start_time: time
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-    });
-});
-
-typer.onkeyup =  async (e)=>{
-    let text = e.target.value;
+document.onkeyup =  async (e)=>{
+    if (e.ctrlKey || e.altKey || e.metaKey) return;
+    if (["Shift","Control","Alt","Meta","OS","Tab", "Enter","Escape"].includes(e.key)) return;
+    if (e.key == 'Backspace') {
+        typer.textContent = typer.textContent.substring(0, typer.innerText.length - 1);
+        document.getElementById('prompt').innerHTML = highlight(typer.textContent, document.getElementById('prompt').dataset.prompt);
+        return;
+    }
+    if (e.key == ' ') {
+        typer.textContent += ' ';
+    } else {
+        typer.textContent += e.key;
+    }
+    let text = typer.textContent;
     let keyPressed = {
         key: e.key,
         location: e.location,
@@ -28,6 +23,18 @@ typer.onkeyup =  async (e)=>{
     };
     console.log(keyPressed);
     document.getElementById('prompt').innerHTML = highlight(text, document.getElementById('prompt').dataset.prompt);
+    
+    await fetch('/times', {
+        method: 'POST',
+        body: JSON.stringify({
+            event: keyPressed,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(function (response) {
+        return response.json();
+    })
     
     if (text.trim() == document.getElementById('prompt').dataset.prompt.trim()) {
         let time = new Date().getTime();
@@ -42,29 +49,15 @@ typer.onkeyup =  async (e)=>{
         }).then(function (response) {
             return response.json();
         }).then(function (data) {
-            document.getElementById('flag').innerText = data.flag ?? "too slow!";
+            document.getElementById('flag').innerText = data.flag ?? data.status ?? "too slow!";
         });
 
-        alert('done');
-        typer.onkeyup = null;
-        
-    } else {
-        await fetch('/times', {
-            method: 'POST',
-            body: JSON.stringify({
-                event: keyPressed,
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(function (response) {
-            return response.json();
-        })
+        typer.onkeyup = null;        
     }
 }
 
 function highlight(substr, str) {
-    if (!str.includes(substr)) return str;
+    if (!str.startsWith(substr)) return str;
     let index = str.indexOf(substr);
     return "<span class='right'>" + substr + "</span>" + str.substring(index + substr.length);
 }
