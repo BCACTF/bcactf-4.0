274b800183716dc75741ecea7045656d646a8d2b
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-06-07 22:02:19 -0400

    committing for jack Co-authored-by: Jack Crowley <Jack-Crowley@users.noreply.github.com>

--- type-faster/server.py
+++ type-faster/server.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import json
 from datetime import datetime
 
@@ -8,47 +8,53 @@ start_time = None
 flag_f = open("flag.txt", "r")
 flag = flag_f.read()
 flag_f.close()
+app.secret_key = 'bcactfhasaverysecurestringofsessiontokenkeyshahahaha2w9un2rind3h4db'
 
-WORD_COUNT = 75
-chars_inp = []
-timeses = []
+WORD_COUNT = 3
 
 @app.route("/")
 def index():
-    global char_list
-    global chars_inp
-    timeses.clear()
-    word_file = open("words.txt", "r")
-    words = word_file.read().split("\n")
-    word_list = [words[random.randint(0, len(words) - 1)] for i in range(WORD_COUNT)]
-    char_list = list(" ".join(word_list))
-    chars_inp = []
+    if not session.get("timeses"):
+        print("Hello")
+        session["timeses"] = [100000]
+        session["chars_inp"] = []
+        word_file = open("words.txt", "r")
+        words = word_file.read().split("\n")
+        word_list = [words[random.randint(0, len(words) - 1)] for i in range(WORD_COUNT)]
+        session["char_list"] = list(" ".join(word_list))
+    
     return render_template("index.html", words=" ".join(word_list))
 
 @app.route("/times", methods=["POST"])
 def times():
-    
     if "end" in request.data.decode("utf-8"):
-        end_time = timeses[-1]
-        start_time = timeses[0]
+        if session["timeses"] == []:
+            # print("ERROR")
+            # print(session)
+            return {"status": "Failed"}
+        start_time = session["timeses"][0]
+        end_time = session["timeses"][-1]
         diff = end_time - start_time
-        print(diff)
-        if diff <= 8000 and len(chars_inp) == len(char_list):
+        # print(diff)
+        if diff <= 8000 and len(session["chars_inp"]) == len(session["char_list"]):
             return {"flag": flag}
         
-        return {"status": "idk"}
+        return {"status": "ok"}
     
     elif "event" in request.data.decode("utf-8"):
         event = json.loads(request.data.decode('utf-8'))['event']
-        print(event)
-        timeses.append(event["when"])
+        # print(event)
+        chars_inp = session["chars_inp"]
+        session["timeses"].append(event["when"])
+        print(f"TimeStamps: {','.join([str(i) for i in session['timeses']])}")
         if event["key"] in "abcdefghijklmnopqrstuvwxyz .,":
             if event["key"] == " " and chars_inp[-1] == " ":
-                return {"status": "idk"}
-            chars_inp.append(event["key"])
+                return {"status": "ok"}
+            session["chars_inp"].append(event["key"])
+            # print(session["chars_inp"])
         if event["key"] == "Backspace":
-            chars_inp.pop()
-        return {"status": "idk"}
+            session["chars_inp"].pop()
+        return {"status": "ok"}
     else:
         return {"status": "error"}
 
