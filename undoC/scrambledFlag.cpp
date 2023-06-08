2963bf5bd304d8424dcb4f2a6418a38e19018c33
Jacob Berger <tkdpenguin123@gmail.com>
2023-06-07 22:04:21 -0400

    combined it into one file

--- undoC/scrambledFlag.cpp
+++ undoC/scrambledFlag.cpp
@@ -2,19 +2,37 @@
 using namespace std;
 
 int main() {
-    freopen("flag.txt","r",stdin);
-    string flag;
-    std::cin >> flag;
-    string scrambled = "";
-    while (flag.size() > 0) {
-        char c = flag.at(flag.size()/2);
-        scrambled.push_back(c);
-        std::cout << scrambled << endl;
-        flag.erase(flag.size()/2,1);
+    FILE *flagFile = fopen("flag.txt","r");
+    char flag[50];
+    fgets(flag, 50, flagFile);
+    string baseFlag = "";
+    for (char c : flag) {
+        baseFlag += c;
     }
-    for (int i = 0; i < scrambled.size(); i++) {
-        std::cout << scrambled.at(i)*3+5-24/2;
-        std::cout << "  ";
+    int encryptedFlag[50] = {};
+    int count = 0;
+    while (baseFlag.size() > 0) {
+        char c = baseFlag.at(baseFlag.size()/2);
+        encryptedFlag[count] =  c*3+5-24/2;
+        count++;
+        baseFlag.erase(baseFlag.size()/2,1);
     }
-    return 0;
+
+    fclose(flagFile);
+
+    int userInput[50] = {};
+    for (int i = 0; i < 50; i++) {
+        int input;
+        std::cin >> input;
+        userInput[i] = input;
+    }
+
+    bool correct = true;
+    for (int i = 0; i < 50; i++) {
+        if (userInput[i] != encryptedFlag[i]) {
+            correct = false;
+        }
+    }
+    if (correct) {std::cout << "Correct!";}
+    else {std::cout << "Incorrect.";}
 }
