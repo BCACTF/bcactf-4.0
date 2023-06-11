be4d1d43292733404b4183f0bac3e5fdb09cf8a8
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-06-10 22:01:13 -0400

    fix session thingy
    Co-authored-by: Jack Crowley <Jack-Crowley@users.noreply.github.com>

--- type-faster/server/server.py
+++ type-faster/server/server.py
@@ -18,8 +18,9 @@ WORD_COUNT = 70
 
 @app.route("/")
 def index():
-    session["timeses"] = session["timeses"] if "timeses" in session.keys() else []
-    session["chars_inp"] = session["chars_inp"] if "chars_inp" in session.keys() else []
+    [session.pop(k) for k in list(session.keys())]
+    session["timeses"] = []
+    session["chars_inp"] = []
     word_file = open("words.txt", "r")
     words = word_file.read().split("\n")
     word_list = [words[random.randint(0, len(words) - 1)] for i in range(WORD_COUNT)]
@@ -35,7 +36,7 @@ def times():
         start_time = session["timeses"][0]
         end_time = session["timeses"][-1]
         diff = end_time - start_time
-        if diff <= 8000 and len(session["chars_inp"]) == len(session["char_list"]):
+        if diff <= 13000 and len(session["chars_inp"]) == len(session["char_list"]):
             return {"flag": flag}
         print(end_time, start_time, diff)
         print(len(session["chars_inp"]), len(session["char_list"]))
